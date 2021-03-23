4c87c148561576d00dc67f4eb783add076053eb3
Chris <34993144+Mesiow@users.noreply.github.com>
2021-03-23 15:17:29 -0400

    switch to C compiler

--- VaultNet/vn.c
+++ VaultNet/vn.c
@@ -9,39 +9,39 @@ int32_t vnNetworkInit()
     return 0;
 }
 
-int32_t vnNetworkHost(vnNetworkServer& host, uint16_t port, uint32_t max_clients)
+int32_t vnNetworkHost(vnNetworkServer* host, uint16_t port, uint32_t max_clients)
 {
     //Bind server
-    host.address.host = ENET_HOST_ANY;
-    host.address.port = port;
+    host->address.host = ENET_HOST_ANY;
+    host->address.port = port;
 
-    host.host = enet_host_create(
-        &host.address, max_clients,
+    host->host = enet_host_create(
+        &host->address, max_clients,
         2, //2 Channels
         0,
         0
     );
 
-    if (host.host == nullptr) {
-        std::cerr << "Error occured creating a vnNetwork Host\n";
+    if (host->host == NULL) {
+        printf("Error occured creating a vnNetwork Host\n");
         return -1;
     }
     
     return 0;
 }
 
-int32_t vnNetworkConnect(vnNetworkPeer& peer, const char* addressToConnect, uint16_t port)
+int32_t vnNetworkConnect(vnNetworkPeer* peer, const char* addressToConnect, uint16_t port)
 {
-    peer.host = enet_host_create(
-        nullptr,
+    peer->host = enet_host_create(
+        NULL,
         1, //Single peer
         2, //2 Channels
         0,
         0
     );
 
-    if (!peer.host) {
-        std::cerr << "Error occured creating a client\n";
+    if (!peer->host) {
+        printf("Error occured creating a client\n");
         return -1;
     }
 
@@ -54,23 +54,23 @@ int32_t vnNetworkConnect(vnNetworkPeer& peer, const char* addressToConnect, uint
     addr.port = port;
 
     //Connect client to the specified address and port
-    peer.peer = enet_host_connect(peer.host, &addr, 2, 0);
-    if (peer.peer == nullptr) {
-        std::cerr << "No available peers for creating a connection(client)\n";
+    peer->peer = enet_host_connect(peer->host, &addr, 2, 0);
+    if (peer->peer == NULL) {
+        printf("No available peers for creating a connection(client)\n");
         return -1;
     }
 
     ENetEvent ev;
-    if (enet_host_service(peer.host, &ev, 3000) > 0 &&
+    if (enet_host_service(peer->host, &ev, 3000) > 0 &&
         ev.type == ENET_EVENT_TYPE_CONNECT) {
-        std::cout << "Connection to " << addressToConnect << ":" << port << " succeeded\n";
-        enet_host_flush(peer.host);
+        printf("Connection to %s:%d has succeeded", addressToConnect, port);
+        enet_host_flush(peer->host);
     }
     else {
-        std::cerr << "Connection attempt timed out\n";
+        printf("Connection attempt timed out\n");
         //Either the 3 seconds are up or a disconnect event was received.
         //Reset the peer in the event the 3 seconds had run out
-        enet_peer_reset(peer.peer);
+        enet_peer_reset(peer->peer);
         return -1;
     }
 
@@ -87,7 +87,7 @@ void vnNetworkSendPacket(const char* data, uint32_t size)
 
 }
 
-void vnNetworkSendPacketTo(const vnNetworkPeer& peer, const char* data, uint32_t size)
+void vnNetworkSendPacketTo(const vnNetworkPeer* peer, const char* data, uint32_t size)
 {
 
 }
--- VaultNet/vn.h
+++ VaultNet/vn.h
@@ -3,34 +3,34 @@
 #ifndef VN_H
 
 #include <enet/enet.h>
-#include <cstdint>
-#include <iostream>
+#include <stdint.h>
+#include <stdio.h>
 
-struct vnNetworkServer {
+typedef struct vnNetworkServer {
 	ENetHost* host;
 	ENetAddress address;
 	uint32_t connections;
-};
+}vnNetworkServer;
 
-struct vnNetworkPeer {
+typedef struct vnNetworkPeer {
 	ENetHost* host;
 	ENetPeer* peer;
 	uint32_t id;
-};
+}vnNetworkPeer;
 
-struct vnNetworkData {
+typedef struct vnNetworkData {
 	vnNetworkPeer sender;
 	uint8_t* data;
 	uint32_t length;
-};
+}vnNetworkData;
 
 int32_t vnNetworkInit();
-int32_t vnNetworkHost(vnNetworkServer &host, uint16_t port, uint32_t max_clients);
-int32_t vnNetworkConnect(vnNetworkPeer &peer, const char* addressToConnect, uint16_t port);
+int32_t vnNetworkHost(vnNetworkServer *host, uint16_t port, uint32_t max_clients);
+int32_t vnNetworkConnect(vnNetworkPeer *peer, const char* addressToConnect, uint16_t port);
 
 void vnNetworkBroadcast(const char* data, uint32_t size);
 void vnNetworkSendPacket(const char* data, uint32_t size);
-void vnNetworkSendPacketTo(const vnNetworkPeer& peer, const char* data, uint32_t size);
+void vnNetworkSendPacketTo(const vnNetworkPeer* peer, const char* data, uint32_t size);
 void vnNetworkDisconnect();
 void vnNetworkCleanup();
 
