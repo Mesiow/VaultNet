5454da647adb11e3cd7d57c46cdacbd75da82161
Chris <34993144+Mesiow@users.noreply.github.com>
2021-03-27 19:13:13 -0400

    add extern c to allow C linkage

--- VaultNet/vn.h
+++ VaultNet/vn.h
@@ -7,82 +7,90 @@
 #include <stdio.h>
 #include <assert.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define MAX_NETWORK_EVENTS 10
 
-typedef enum eNetworkEvent {
-	NetworkEventConnected,
-	NetworkEventDisconnected
-}eNetworkEvent;
+	typedef enum eNetworkEvent {
+		NetworkEventConnected,
+		NetworkEventDisconnected
+	}eNetworkEvent;
 
-typedef struct vnNetworkServer {
-	ENetHost* host;
-	ENetAddress address;
-	uint32_t connections;
-}vnNetworkServer;
+	typedef struct vnNetworkServer {
+		ENetHost* host;
+		ENetAddress address;
+		uint32_t connections;
+	}vnNetworkServer;
 
-typedef struct vnNetworkPeer {
-	ENetHost* host;
-	ENetPeer* peer;
-	uint32_t id;
-}vnNetworkPeer;
+	typedef struct vnNetworkPeer {
+		ENetHost* host;
+		ENetPeer* peer;
+		uint32_t id;
+	}vnNetworkPeer;
 
-typedef struct vnNetworkData {
-	vnNetworkPeer *sender;
-	uint8_t* data;
-	uint32_t length;
-}vnNetworkData;
-
-
-
-/*
-	Initialize the library
-*/
-int32_t vnNetworkInit();
-
-/*
-	Create a network server/host at a specific port and allow max_clients
-	number of connections
-*/
-int32_t vnNetworkHost(uint16_t port, uint32_t max_clients);
+	typedef struct vnNetworkData {
+		vnNetworkPeer* sender;
+		uint8_t* data;
+		uint32_t length;
+	}vnNetworkData;
 
 
-/*
-	Connect a client to a remote host
-*/
-int32_t vnNetworkConnect(const char* addressToConnect, uint16_t port);
+
+	/*
+		Initialize the library
+	*/
+	int32_t vnNetworkInit();
+
+	/*
+		Create a network server/host at a specific port and allow max_clients
+		number of connections
+	*/
+	int32_t vnNetworkHost(uint16_t port, uint32_t max_clients);
 
 
-/*
-	Gets latest received network data
-*/
-vnNetworkData* vnNetworkEventLatest();
+	/*
+		Connect a client to a remote host
+	*/
+	int32_t vnNetworkConnect(const char* addressToConnect, uint16_t port);
 
-/*
-	Poll all network events
-*/
-void vnNetworkPollEvents();
 
-/*
-	Broadcast a packet to all peers
-*/
-void vnNetworkBroadcast(const char* data, uint32_t size);
+	/*
+		Gets latest received network data
+	*/
+	vnNetworkData* vnNetworkEventLatest();
 
-/*
-	Send a packet to the server
-*/
-void vnNetworkSendPacket(const char* data, uint32_t size);
+	/*
+		Poll all network events
+	*/
+	void vnNetworkPollEvents();
 
-/*
-	Send a packet as the server to a connected peer
-*/
-void vnNetworkSendPacketTo(const vnNetworkPeer* peer, const char* data, uint32_t size);
+	/*
+		Broadcast a packet to all peers
+	*/
+	void vnNetworkBroadcast(const char* data, uint32_t size);
 
-int32_t vnNetworkDisconnect();
-void vnNetworkCleanup();
+	/*
+		Send a packet to the server
+	*/
+	void vnNetworkSendPacket(const char* data, uint32_t size);
 
-/*
-	Must be called after done reading a newly received packet
-*/
-void vnNetworkFreeLatestData();
+	/*
+		Send a packet as the server to a connected peer
+	*/
+	void vnNetworkSendPacketTo(const vnNetworkPeer* peer, const char* data, uint32_t size);
+
+	int32_t vnNetworkDisconnect();
+	void vnNetworkCleanup();
+
+	/*
+		Must be called after done reading a newly received packet
+	*/
+	void vnNetworkFreeLatestData();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // !VN_H
